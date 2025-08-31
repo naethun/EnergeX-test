d353f39c5052ae77d9b3d4ea2de17f10bb808e5d
nathan <natraniel2@gmail.com>
2025-08-30 22:24:38 -0700

    basic jest test

---
+++ cache-node/src/index.test.ts
@@ -0,0 +1,9 @@
+import request from 'supertest';
+import express from 'express';
+
+// Simple test to ensure Jest works
+describe('Cache Node API', () => {
+  test('should pass basic test', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
